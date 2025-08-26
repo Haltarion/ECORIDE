a2a2bb233af938cd309722477498b86fcdcedf3f
Haltarion <michael.bourloux@gmail.com>
2025-08-26 15:11:16 +0200

    Auto commit

--- pages/covoiturages.php
+++ pages/covoiturages.php
@@ -53,7 +53,7 @@
     </div>
     <div class="covoiturages__button">
         <?php include '../components/clicable/button.php' ?>
-            </div>
+    </div>
     
     
 <?php include '../common/footer.php'; ?>
