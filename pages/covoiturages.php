17b2d98fe841b7ba16aa1f8ff3c6766d9e8101e6
Haltarion <michael.bourloux@gmail.com>
2025-08-26 15:01:08 +0200

    Auto commit

--- pages/covoiturages.php
+++ pages/covoiturages.php
@@ -14,44 +14,44 @@
       ?>
       
       <?php
-      $labelMain = "Quelle est votre date de départ ?";
-      $labelDay = "Jour";
-      $placeholderDay = "DD";
-      $labelMonth = "Mois";
-      $placeholderMonth = "MM";
-      $labelYear = "Année";
-      $placeholderYear = "AAAA";
-      $alertMessage = "Merci de rentrer la date au format DD MM AAAA.";
-      $inputId = "dateDepart";
-      include '../components/form/input-date.php';
+        $labelMain = "Quelle est votre date de départ ?";
+        $labelDay = "Jour";
+        $placeholderDay = "DD";
+        $labelMonth = "Mois";
+        $placeholderMonth = "MM";
+        $labelYear = "Année";
+        $placeholderYear = "AAAA";
+        $alertMessage = "Merci de rentrer la date au format DD MM AAAA.";
+        $inputId = "dateDepart";
+        include '../components/form/input-date.php';
       ?>
     </fieldset>
     <fieldset class="covoiturages__arrivee">
       <legend>Arrivée</legend>
       <?php
-      $label = "Quelle est votre adresse d'arrivée ?";
-      $placeholder = "Adresse d'arrivée";
-      $alertMessage = "Veuillez entrer une adresse d'arrivée valide.";
-      $inputId = "adresseArrivee";
-      include '../components/form/input-text.php';
+        $label = "Quelle est votre adresse d'arrivée ?";
+        $placeholder = "Adresse d'arrivée";
+        $alertMessage = "Veuillez entrer une adresse d'arrivée valide.";
+        $inputId = "adresseArrivee";
+        include '../components/form/input-text.php';
       ?>
-            
-            <?php
-            $labelMain = "Quelle est votre date d'arrivée ?";
-            $labelDay = "Jour";
-            $placeholderDay = "DD";
-            $labelMonth = "Mois";
-            $placeholderMonth = "MM";
-            $labelYear = "Année";
-            $placeholderYear = "AAAA";
-            $alertMessage = "Merci de rentrer la date au format DD MM AAAA.";
-            $inputId = "dateArrivee";
-            include '../components/form/input-date.php';
-            ?>
-        </fieldset>
-        <div class="container__button">
-            <?php include '../components/clicable/button.php' ?>
-        </div>
+      
+      <?php
+        $labelMain = "Quelle est votre date d'arrivée ?";
+        $labelDay = "Jour";
+        $placeholderDay = "DD";
+        $labelMonth = "Mois";
+        $placeholderMonth = "MM";
+        $labelYear = "Année";
+        $placeholderYear = "AAAA";
+        $alertMessage = "Merci de rentrer la date au format DD MM AAAA.";
+        $inputId = "dateArrivee";
+        include '../components/form/input-date.php';
+      ?>
+      </fieldset>
+      <div class="container__button">
+        <?php include '../components/clicable/button.php' ?>
+      </div>
     </div>
     
     
