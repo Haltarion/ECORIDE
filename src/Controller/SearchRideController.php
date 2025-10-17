d81250b05f8e8eb018b247c8fc0cb2f26a89de78
Haltarion <michael.bourloux@gmail.com>
2025-10-17 17:19:37 +0200

    nouveau commit

--- src/Controller/SearchRideController.php
+++ src/Controller/SearchRideController.php
@@ -8,14 +8,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SearchRideController extends AbstractController
 {
-  #[Route('/search-ride', name: 'app_search_ride', methods:['POST'])]
+  #[Route('/search-ride', name: 'app_search_ride', methods:['GET','POST'])]
   public function searchRide(Request $request): Response
   {
-    // Récupération de la ville d'arrivée depuis POST
-    $destinationArrivee = $request->request->get('destination');
+    // Récupération de la ville d'arrivée depuis GET ou POST.
+    // Use ->get() which reads from any parameter bag (query/post) and
+    // provide null default so page loads even if not provided.
+    $destinationArrivee = $request->get('destination', null);
 
     return $this->render('pages/search-ride.html.twig', [
-      'destinationArrivee'=> $destinationArrivee,
+      'destinationArrivee' => $destinationArrivee,
     ]);
   }
 }
