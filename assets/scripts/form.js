13d3e08f7344e30e719ba3cfea9266acd7ce87cd
Haltarion <michael.bourloux@gmail.com>
2025-10-20 18:28:14 +0200

    validation du formulaire de recherche de covoit

--- assets/scripts/form.js
+++ assets/scripts/form.js
@@ -1,25 +1,39 @@
 // Verification des formulaires
 
-// Vérification des champs requis
-
 // Search-ride.html.twig
-// Récupération des champs
+// Récupération des inputs du formulaire
 const inputAdresseDepart = document.getElementById("adresseDepart");
 const inputDateDepart = document.getElementById("dateDepart");
 const inputAdresseArrivee = document.getElementById("adresseArrivee");
 const inputDateArrivee = document.getElementById("dateArrivee");
-const btnSearchRide = document.getElementById("btnSearchRide");
-const formInscription = document.getElementById("formSearchRide");
 
-// Ajout des écouteurs sur chaque input text
-inputAdresseDepart.addEventListener("keyup", validateRequired);
-inputAdresseArrivee.addEventListener("keyup", validateRequired);
-inputDateDepart.addEventListener("input", validateRequired);
-inputDateArrivee.addEventListener("input", validateRequired);
+const btnSearchRide = document.getElementById("btnSearchRide");
+const formSearchRide = document.getElementById("formSearchRide");
+
+// Ajout d'écoute des événements sur les inputs et le bouton
+inputAdresseDepart.addEventListener("keyup", validateForm);
+inputAdresseArrivee.addEventListener("keyup", validateForm);
+inputDateDepart.addEventListener("input", validateForm);
+inputDateArrivee.addEventListener("input", validateForm);
+
+btnSearchRide.addEventListener("click", AfficherCovoiturages);
+
+//Function permettant de valider tout le formulaire et d'activer le bouton si tout est ok
+function validateForm() {
+    const adresseDepartOk = validateRequired(inputAdresseDepart);
+    const AdresseArriveeOk = validateRequired(inputAdresseArrivee);
+    const DateDepartOk = validateRequired(inputDateDepart);
+    const DateArriveeOk = validateRequired(inputDateArrivee);
+
+    if (adresseDepartOk && AdresseArriveeOk && DateDepartOk && DateArriveeOk) {
+        btnSearchRide.disabled = false;
+    } else {
+        btnSearchRide.disabled = true;
+    }
+}
 
 // Fonction de validation d'un champ
-function validateRequired(event) {
-    const input = event.target;
+function validateRequired(input) {
     if (input.value.trim() != "") {
         input.classList.remove("is-invalid");
         input.classList.add("is-valid");
